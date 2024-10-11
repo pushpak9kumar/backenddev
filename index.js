b4128e9403d586b23463be63b91414e1ae20623b
pushpak <pushpak94681@gmail.com>
2024-10-11 12:30:33 +0530

    This is first commit from push_fuck

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res) => {
+    res.send('pushpak twit')
+})
+
+app.get('/login',(req,res) => {
+    res.send('<h1> please login at backend dev </h1>')
+})
+
+app.get('/youtube', (req,res) => {
+    res.send("<h2>chai aur code</h2>")
+})
+ 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
